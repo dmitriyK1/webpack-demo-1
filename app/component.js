cbac41f96df68c84595b0c53969052d374b6362c
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-12-07 22:52:13 +0200

    add errors

--- app/component.js
+++ app/component.js
@@ -1,7 +1,8 @@
 import "!demo-loader?name=foo!./main.css";
 
 export default (text = "Hello world") => {
-  const element = document.createElement("div");
+const element = document.createElement("div")
+console.log(foo);
 
   element.className = "fa fa-hand-spock-o fa-1g";
   element.innerHTML = text;
